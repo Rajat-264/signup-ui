e8e71a8cf82dd97f1b03ce70d032d84af44fc2d1
Rajat Hande <147739742+Rajat-264@users.noreply.github.com>
2025-03-12 11:22:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",  // Allow external access
-    port: 5173,       // Ensure the correct port
+    port: 80,       // Ensure the correct port
     strictPort: true,
     allowedHosts: ["signup-ui-production.up.railway.app"],  // Allow Railway domain
   },
