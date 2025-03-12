aad7d46bea4c76c37320bafad26c5fa1a418c626
Rajat-264 <handerajat@gmail.com>
2025-03-12 09:30:38 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SignupForm from "./SignupForm";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <h2 className="h2">Signup Form</h2>
+      <SignupForm />
+    </div>
+  );
+}
+
+export default App;
